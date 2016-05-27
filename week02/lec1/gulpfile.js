8cee789c5dc00f0d8d291104b47da569f6dddbc7
Yevs <yevhen.yevsyuhov@gmail.com>
2016-05-28 00:42:42 +0300

    started from scratch

--- week02/lec1/gulpfile.js
+++ week02/lec1/gulpfile.js
@@ -1,6 +1,7 @@
 var gulp = require('gulp');
 var postcss = require('gulp-postcss');
 var less = require('gulp-less');
+var browserSync = require('browser-sync').create();
 
 var autoprefixer = require('autoprefixer');
 var cssnano = require('cssnano');
@@ -14,4 +15,22 @@ gulp.task('css', function () {
         .pipe(less())
         .pipe(postcss(processors))
         .pipe(gulp.dest('.'));
+});
+
+gulp.task('css-watch', ['css'], () => {
+    browserSync.reload();
+});
+gulp.task('browser-reload', () => {
+    browserSync.reload({stream: true});
+});
+
+gulp.task('serve', ['css'], () => {
+    browserSync.init({
+            server: {
+                baseDir: './'
+            },
+            browser: 'google-chrome'
+        });
+    gulp.watch('./*.less', ['css-watch']);
+    gulp.watch('./*.html', browserSync.reload);
 });

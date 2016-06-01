8bb1bc0e72372f427914be1807e206b8156ed166
Yevs <yevhen.yevsyuhov@gmail.com>
2016-06-01 23:44:02 +0300

    deep assign homework

---
+++ week03/lec1/deepAssign.js
@@ -0,0 +1,75 @@
+const isEnumerable = (object, prop) => Object.prototype.propertyIsEnumerable.call(object, prop);
+const isObject = object => object === Object(object);
+
+
+function cloneRegExp(regex) {
+    const pattern = regex.source;
+    let flags = '';
+    if (regex.global) {
+        flags += 'g';
+    }
+    if (regex.ignoreCase) {
+        flags += 'i';
+    }
+    if (regex.multiline) {
+        flags += 'm';
+    }
+    return new RegExp(pattern, flags);
+}
+
+function copy(object) {
+    if (!isObject(object)) {
+        return object;
+    }
+    if (object instanceof Date) {
+        return new Date(object);
+    }
+    if (object instanceof RegExp) {
+        return cloneRegExp(object);
+    }
+
+    let result = new object.constructor();
+    Reflect.ownKeys(object)
+           .filter(key => isEnumerable(object, key))
+           .forEach(key => {
+
+                let value = object[key];
+                result[key] = copy(value);
+
+                if (value instanceof Map) {
+                    for (let [itemKey, itemVal] of value) {
+                        result[key].set(copy(itemKey), copy(itemVal));
+                    }
+                    return;
+                }
+
+                if (value instanceof Set) {
+                    for (let item of value) {
+                        result[key].add(copy(item));
+                    }
+                    return;
+                }
+            });
+    return result;
+}
+
+function deepAssign(target, source) {
+    if (!isObject(source)) {
+        return;
+    }
+    Reflect.ownKeys(source)
+       .filter(key => isEnumerable(source, key))
+       .forEach(key => {
+            target[key] = copy(source[key]);
+       });
+}
+
+
+module.exports = (target, ...sources) => {
+    if (target == null) {
+        throw new TypeError();
+    }
+    sources.forEach(source => {
+        deepAssign(target, source);
+    });
+};
---
+++ week03/lec1/index.js
@@ -0,0 +1,35 @@
+const deepAssign = require('./deepAssign.js');
+
+let date = new Date();
+let source1 = {
+    a: 1,
+    b: 2,
+    c: new Set([1, 2, 3]),
+    [Symbol.for('d')]: 'abcde',
+    e: new Map([[1, 2], [date, 5]]),
+    f: /^abc$/g,
+    g: {
+        a: 3,
+        b: 4
+    },
+    h: date
+};
+let source2 = {b: 3};
+
+console.log('sources:');
+console.log(source1);
+console.log(source2);
+
+let target = Object.create(null);
+
+console.log('target:');
+console.log(target);
+
+deepAssign(target, source1, source2);
+
+console.log(`symbol key value == ${source1[Symbol.for('d')]}`);
+console.log('target:');
+console.log(target);
+console.log(`symbol key value == ${target[Symbol.for('d')]}`);
+console.log(`are dates equal? - ${source1.h.getTime() === target.h.getTime()}`);
+console.log(`is it a deep copy? - ${!(source1.g == target.g)}`);

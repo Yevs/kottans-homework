f9b10d587da18a64b02c2d1c425de9cfa6b0e630
Yevs <yevhen.yevsyuhov@gmail.com>
2016-06-02 23:08:57 +0300

    fixed Object.create(null) bug

--- week03/lec1/helpers/isEnumerable.js
+++ week03/lec1/helpers/isEnumerable.js
@@ -1,1 +1,2 @@
-module.exports = (object, prop) => object.propertyIsEnumerable(prop);
+// calling through prototype due to Object.create(null)
+module.exports = (object, prop) => Object.prototype.propertyIsEnumerable.call(object, prop);
